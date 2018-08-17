502cacc853c19d0e7baa6d29d9178df3bcd67762
goxp49 <215920363@qq.com>
2018-08-17 18:27:11 +0800

    初始化。

--- creat_images.py
+++ creat_images.py
@@ -13,3 +13,29 @@ def example_1():
     # creat color image
     bgrImage = numpyArray.reshape(100, 400, 3)
     cv2.imwrite('ColorImage.png', bgrImage)
+
+#========================================== 显示图片==========================================
+def example_2():
+    img = cv2.imread('ColorImage.png')
+    cv2.imshow('Image', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+#========================================== 捕获摄像头内容 ==========================================
+def example_3():
+    cameraCapture = cv2.VideoCapture(0)
+    fps = 30 #  这是个假设值
+    size = (int(cameraCapture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cameraCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    videoWriter = cv2.VideoWriter('OutputVid.avi', cv2.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
+
+    success, frame = cameraCapture.read()
+    numFrames = 10 * fps - 1
+    while success and numFrames > 0:
+        videoWriter.write(frame)
+        success, frame = cameraCapture.read()
+        numFrames -= 1
+    cameraCapture.release()
+
+if __name__ == "__main__":
+    example_3()
